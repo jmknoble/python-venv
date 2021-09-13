fa6195cb5480bad437e7850611bb74ac5be025fb
Jim Knoble <jmknoble@pobox.com>
2021-09-13 10:36:15 -0700

    Improve example install scripts

--- examples/install-pyenv.sh
+++ examples/install-pyenv.sh
@@ -3,8 +3,17 @@
 set -e
 set -u
 
+# This uses [python-venv](https://github.com/jmknoble/python-venv)
+# to create a Python virtual environmeent using 'pyenv'.
+#
 # Call this script with '-n' or '--dry-run' for a dry run
+
+ENV_NAME="${ENV_NAME:-}"
 
 set -x
 
-./python-venv replace -t pyenv -r wheel ${1:+"$@"}
+./python-venv replace \
+    -t pyenv \
+    -r wheel \
+    ${ENV_NAME:+-e "${ENV_NAME}"} \
+    ${1:+"$@"}
--- examples/install-venv.sh
+++ examples/install-venv.sh
@@ -3,9 +3,20 @@
 set -e
 set -u
 
+# This uses [python-venv](https://github.com/jmknoble/python-venv)
+# to create a Python virtual environmeent using 'python3 -m venv'.
+#
 # Call this script with '-n' or '--dry-run' for a dry run
 
-VENV_DIR="${HOME}/.venvs"
+ENV_NAME="${ENV_NAME:-python-venv}"
+VENV_DIR="${VENV_DIR:-${HOME}/.venvs}"
+USE_PYTHON="${USE_PYTHON:-/usr/bin/python3}"
+
+case "${USE_PYTHON}" in
+    none|default)
+        USE_PYTHON=""
+        ;;
+esac
 
 set -x
 
@@ -14,6 +25,6 @@ mkdir -p "${VENV_DIR}"
 ./python-venv replace \
     -t venv \
     -r wheel \
-    -e "${VENV_DIR}/python-venv" \
-    --python /usr/bin/python3 \
+    -e "${VENV_DIR}/${ENV_NAME}" \
+    ${USE_PYTHON:+--python "${USE_PYTHON}"} \
     ${1:+"$@"}
