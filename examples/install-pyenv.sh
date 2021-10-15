698aff4341c358b0e1469c845398f275a3df1cb8
Jim Knoble <jmknoble@pobox.com>
2021-10-15 14:49:40 -0700

    Update example install scripts

--- examples/install-pyenv.sh
+++ examples/install-pyenv.sh
@@ -6,14 +6,12 @@ set -u
 # This uses [python-venv](https://github.com/jmknoble/python-venv)
 # to create a Python virtual environmeent using 'pyenv'.
 #
-# Call this script with '-n' or '--dry-run' for a dry run
-
-ENV_NAME="${ENV_NAME:-}"
+# Call this script with '-n' or '--dry-run' for a dry run,
+# or with '-e' or '--env-name' to change the environment name.
 
 set -x
 
 ./python-venv replace \
     -t pyenv \
     -r wheel \
-    ${ENV_NAME:+-e "${ENV_NAME}"} \
     ${1:+"$@"}
--- examples/install-venv.sh
+++ examples/install-venv.sh
@@ -6,17 +6,12 @@ set -u
 # This uses [python-venv](https://github.com/jmknoble/python-venv)
 # to create a Python virtual environmeent using 'python3 -m venv'.
 #
-# Call this script with '-n' or '--dry-run' for a dry run
-
-ENV_NAME="${ENV_NAME:-python-venv}"
-VENV_DIR="${VENV_DIR:-${HOME}/.venvs}"
+# Call this script with '-n' or '--dry-run' for a dry run,
+# or with '-e' or '--env-name' to change the environment name.
 
 set -x
 
-mkdir -p "${VENV_DIR}"
-
 ./python-venv replace \
-    -t venv \
+    -t named-venv \
     -r wheel \
-    -e "${VENV_DIR}/${ENV_NAME}" \
     ${1:+"$@"}
